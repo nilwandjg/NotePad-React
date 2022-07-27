f3be6c4de3b04ae4d7e7e67984aadfdfc96b7f01
Nilwan de Jesus Grisosti <nilwandjg@yahoo.com.br>
2022-07-27 00:25:02 -0300

    primeiro commit - desenvolvido até o header

---
+++ src/components/Task.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./Task.css";
+import { CgClose } from "react-icons/cg";
+
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
+    return (
+        <div
+            className="task-container"
+            style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
+        >
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+
+            <div className="buttons-container">
+                <button
+                    className="remove-task-button"
+                    onClick={() => handleTaskDeletion(task.id)}
+                >
+                    <CgClose />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Task;
